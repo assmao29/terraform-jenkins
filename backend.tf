5ab326a80279727e19a1e39cb42e78196d39579a
assmao29 <144760217+assmao29@users.noreply.github.com>
2024-03-06 18:48:46 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "maos3-bucket"
-    key            = "week12/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraformLock"
